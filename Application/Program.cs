6276125df1fcd432e92eb6b1bfcf9c85c9d5cd2d
Александар Ранковић <rankovica02@gmail.com>
2026-01-08 12:46:00 +0100

    Registracija korisnika, dodavanje Logger Servisa

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,17 +1,19 @@
 ﻿using Database.Repozitorijumi;
 using Domain.BazaPodataka;
+using Domain.Enumeracije;
 using Domain.Modeli;
 using Domain.Repozitorijumi;
 using Domain.Servisi;
 using Presentation.Authentifikacija;
 using Presentation.Meni;
 using Services.AutenftikacioniServisi;
+using Services.LoggerServisi;
 
 namespace Loger_Bloger
 {
     public class Program
     {
-        public static void Main() //komentar jevta proba
+        public static void Main() //komentar 
         {
             // Baza podataka
             IBazaPodataka bazaPodataka = null; // TODO: Initialize the database with appropriate implementation
@@ -19,13 +21,18 @@ namespace Loger_Bloger
             // Repozitorijumi
             IKorisniciRepozitorijum korisniciRepozitorijum = new KorisniciRepozitorijum(bazaPodataka);
 
+
+
             // Servisi
-            IAutentifikacijaServis autentifikacijaServis = new AutentifikacioniServis(); // TODO: Pass necessary dependencies
+            ILoggerServis loggerServis = new FileLoggerServis();
+            IAutentifikacijaServis autentifikacijaServis = new AutentifikacioniServis(korisniciRepozitorijum, loggerServis); // TODO: Pass necessary dependencies
             // TODO: Add other necessary services
 
-            // Ako nema nijednog korisnika u sistemu, dodati dva nova
+            // Ako nema nijednog korisnika u sistemu,dodati dva nova
             if (korisniciRepozitorijum.SviKorisnici().Count() == 0)
             {
+                korisniciRepozitorijum.DodajKorisnika(new Korisnik("mare123", "sifra123", "Marko Markovic", TipKorisnika.GlavniEnolog));
+                korisniciRepozitorijum.DodajKorisnika(new Korisnik("pera321", "sifra321", "Pera Peric", TipKorisnika.KelarMajstor));
                 // TODO: Add initial users to the system
             }
 
