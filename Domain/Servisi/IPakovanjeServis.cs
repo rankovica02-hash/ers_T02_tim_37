17907c76adfe9bd0847c76f4aa58b0fac4451bb8
Andjelija Lukic <lukicandja4@gmail.com>
2026-01-15 12:17:56 +0100

    Zavrsen servis pakovanja

---
+++ Domain/Servisi/IPakovanjeServis.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enumeracije;
+using Domain.Modeli;
+namespace Domain.Servisi
+{
+    public interface IPakovanjeServis
+    {
+        Paleta SpakujVinaUNovuPaletu(KategorijaVina kategorija, int brojFlasa, ZapreminaFlase zapremina, string nazivSorte, string adresaOdredista, long vinskiPodrumId);
+        Paleta PosaljiPaletuUVinskiPodrum(KategorijaVina kategorija, int brojFlasa, ZapreminaFlase zapremina, string nazivSorte, string adresaOdredista, long vinskiPodrumId);
+    }
+}
